5811d63fffef8aff62cc86add035194bf86b2ca3
crumpj4 <crumpj4@gmatc.matc.edu>
2021-03-15 18:51:38 -0500

    Completed Project1

---
+++ script.js
@@ -0,0 +1,16 @@
+
+//Taste Notes
+
+var taste1 = 'Bold';
+var taste2 = 'Earthy';
+var taste3 = 'Blend';
+
+var t1 = document.getElementById('t1');
+var t2 = document.getElementById('t2');
+var t3 = document.getElementById('t3');
+
+t1.textContent = taste1;
+t2.textContent = taste2;
+t3.textContent = taste3;
+
+
